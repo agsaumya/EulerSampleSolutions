51cb2a546ec783b5bfa5cde9d7d0e01a88b1ae09
Saumya Agarwal <ag.saumya2@gmail.com>
2017-09-11 09:05:55 -0700

    Updating the comments

--- AdobeProblemSet/src/saumya/dev/Problem3.java
+++ AdobeProblemSet/src/saumya/dev/Problem3.java
@@ -9,7 +9,7 @@ public class Problem3 {
 
     /**
      * Problem 20 Factorial digit sum
-		https://projecteuler.net/problem=47
+		https://projecteuler.net/problem=20
      * n! means n × (n − 1) × ... × 3 × 2 × 1
 
 		For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
